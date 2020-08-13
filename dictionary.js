505a3d911ac21b0a88ea18bfce5f68268124c0b3
John <dongyongkim8@gmail.com>
2020-08-13 02:41:07 -0400

    finished function to search for words that are almost identical phoneme

--- dictionary.js
+++ dictionary.js
@@ -1,42 +1,10 @@
-/*document.getElementById('file').onchange = function () {
 
-    var file = this.files[0];
-
-    var reader = new FileReader();
-    reader.onload = function (progressEvent) {
-        // Entire file
-        console.log(this.result);
-
-        // By lines
-        var lines = this.result.split('\n');
-        for (var line = 0; line < lines.length; line++) {
-            console.log(lines[line]);
-        }
-    };
-    reader.readAsText(file);
-};
-//let W = prompt("Please enter a word");
-
-let pronunciation = "";
-let identical = [];
-let replacePhoneme = [];
-let addPhoneme = [];
-let removePhoneme = [];
-
-function findLines() {
-    var patt1 = /\s/g;
-    var result = str.match(patt1);
-    console.log(result);
-}
-
-*/
-//-----------------------------------------------------------
-
-//let results;
 let fileArray;//array of the whole file
 let filteredArray = [];//a file array with no special charactes
 let userInput = "";// stores the user input from prompt
-let matchingPhenomes = [];
+let matchingSounds = []; //an array of the matching sounding words
+let searchAnswer;//the return value of the initial search
+let addPhoneme = [];//contains the phase III add phoneme
 
 //creates an event listener on the upload file button in HTML
 document.getElementById('fileinput').addEventListener('change', readSingleFile, false);
@@ -63,21 +31,13 @@ function readSingleFile(uploadedFile) {
 }
 
 /**
- * 
+ * parces through a .txt file and stores each new line in the document as an element
+ * in an array
  * @param {*} fileContentLoaded 
  */
 function mainEntry(fileContentLoaded) {
-    //        this is an example use
     console.log("Where the content is: ", fileContentLoaded.split("\n"))
     fileArray = fileContentLoaded.split("\n");//seperates elements in array at the new line
-    //call method
-    //do logic
-
-
-    //results = splitOnSpace(fileArray[0]);
-
-    //searchAllLines();
-    //console.log(result)
 }
 
 /**
@@ -87,18 +47,18 @@ function mainEntry(fileContentLoaded) {
  */
 function splitOnSpace(line) {
     // reset strings
-    var obj = {
+    var wordLineObjects = {
         before: "",
         after: ""
     }
     // accumulate before space
     var i = 0;
-    while (i < line.length && line[i] != " ") { obj.before += line[i]; i++; }
+    while (i < line.length && line[i] != " ") { wordLineObjects.before += line[i]; i++; }
     // skip the space
     i++;
     // accumulate after space
-    while (i < line.length) { obj.after += line[i]; i++; }
-    return obj;
+    while (i < line.length) { wordLineObjects.after += line[i]; i++; }
+    return wordLineObjects;
 }
 
 /**
@@ -106,26 +66,29 @@ function splitOnSpace(line) {
  * input
  */
 function startSearch() {
-    let letters1 = /^[A-Za-z]+$/
+    const letters1 = /^[A-Za-z]+$/;
     while (userInput == 0 || !userInput.match(letters1)) {
         userInput = prompt("Please enter a word");
     }
     filerArray();
-    let result = searchAllLines(userInput);
-    if (result != undefined) {
-        searchAllLinesPhem(result.after);
+    searchAnswer = searchAllLines(userInput);
+    if (searchAnswer != undefined) {
+        searchAllLinesPhem(searchAnswer.after);
     }
+    printResults();
+    searchLinesSimilar(searchAnswer.after);
 }
 
 /**
- * 
- * @param {*} array 
+ * goes through the file string array and checks each line. if a line does not contain
+ * any characters other than letters, numbers, whitespaces, or /'/'s, the line will be
+ * placed in the filtered array
+ * @param {takes an array of strings as an argument} array 
  */
 function filerArray() {
     let x = 0;//iterating through fileArray
     let i = 0;//iterates over filteredArray
-    let filter = /[^a-zA-Z0-9'\s]/;
-    //console.log(filter.test("2  9  9"));
+    const filter = /[^a-zA-Z0-9'\s]/;
     do {
         if (filter.test(fileArray[x]) === false) {
             filteredArray[i] = fileArray[x];
@@ -134,7 +97,7 @@ function filerArray() {
         } else {
             x++;
         }
-    } while (x < 300); //while (x < fileArray.length);
+    } while (x < fileArray.length); //while (x < fileArray.length);
     console.log("filteredArray is " + i + " and fileArray is " + x);
 }
 
@@ -152,34 +115,78 @@ function searchAllLines(searchTerm) {
             continue;
         } else {
             anwser = splitOnSpace(filteredArray[x]);
-            console.log("test found it!! \n >" + anwser.before + "\n Pronunciation:   " + anwser.after);
+            //console.log("test found it!! \n >" + anwser.before + "\n Pronunciation:   " + anwser.after);
             //searchAllLinesPhem(answer.after);
             return anwser;
         }
 
     }
-    console.log("could not find this word...");
+    //console.log("could not find this word...");
 }
 
-
+/**
+ * searches through an array of strings to find matching terms. Will add the matching
+ * word objects to a new array. a match is based off of a match in phenetic spellings
+ * @param {takes in a string of phenetic spelling} phen 
+ */
 function searchAllLinesPhem(phen) {
 
-    console.log("this is the next seach function" + phen);
+    //console.log("this is the next seach function" + phen);
 
-    /*let upperSearch = phen.toUpperCase();
+    let upperSearch = phen.toUpperCase();
     let x = 0;
-    let anwser;//answer from first search of dictionary
+    let i = 0;
+    let localAnswer
     while (filteredArray.length > x) {
         if (filteredArray[x].search(upperSearch) == -1) {
             x++;
             continue;
         } else {
-            anwser = splitOnSpace(filteredArray[x]);
-            console.log("test found it!! \n >" + anwser.before + "\n Pronunciation:   " + anwser.after);
-            //searchAllLinesPhem(answer.after);
-            return anwser;
+            localAnswer = splitOnSpace(filteredArray[x]);
+            matchingSounds[i] = localAnswer;
+            i++;
+            x++;
         }
 
     }
-    console.log("could not find this word...");*/
+}
+
+/**
+ * 
+ * @param {*} phen 
+ */
+function searchLinesSimilar(phen) {
+    let splitPhen = []; //will hold the substring array from the split phen
+    splitPhen = phen.split(" ");
+    console.log(splitPhen.length + "this is the next searasdfch function" + + splitPhen[1] + splitPhen[2] + splitPhen[3]);
+    let x = 0;
+    let numberOfMatches = 0;
+    const lengthOfPhenArray = splitPhen.length;
+    console.log(splitPhen[1]);
+    do {
+        numberOfMatches = 1;
+        splitPhen.forEach(element => {
+            if (filteredArray[x].search(element) != -1 && numberOfMatches != lengthOfPhenArray - 1) {
+                numberOfMatches++;
+            } else if (numberOfMatches == lengthOfPhenArray - 1) {
+                console.log("yesssss match");
+                addPhoneme = filteredArray[x];
+            }
+        });
+        x++;
+    } while (filteredArray.length > x);
+    console.log(addPhoneme.length);
+}
+
+/**
+ * prints the results of the search and its matching phenomes
+ */
+function printResults() {
+    let i = 0;
+    let concatinatedString = "";
+    while (i < matchingSounds.length) {
+        concatinatedString += `${matchingSounds[i].before} `;
+        i++;
+    }
+    console.log(`>${searchAnswer.before} \n \nPronunciation:   ${searchAnswer.after} \n \nIdentical ${concatinatedString}`);
 }
